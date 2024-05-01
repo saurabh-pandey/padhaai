37ed9fc9ecfae3939be2a61b477069f7e1762496
Saurabh Pandey <saurabhpandey85@gmail.com>
2024-05-01 21:42:45 +0530

    TCPL-CH01: Updated the longest line code

--- the_c_prog_lang/ch01/longest_line.c
+++ the_c_prog_lang/ch01/longest_line.c
@@ -2,10 +2,29 @@
 
 #define MAX_LINE_LENGTH 1000
 
-int get_line(char *line) {
+int get_line(char *line, int limit);
+void copy_from_to(char * from, char * to);
+
+int main() {
+    char max_line[MAX_LINE_LENGTH];
+    char line[MAX_LINE_LENGTH];
+    int max_line_length = 0;
+    int line_length = 0;
+    while ((line_length = get_line(line, MAX_LINE_LENGTH)) > 0) {
+        printf("Line = %s, length = %d\n", line, line_length);
+        if (line_length > max_line_length) {
+            copy_from_to(line, max_line);
+            max_line_length = line_length;
+        }
+    }
+    printf("Max line = %s, length = %d\n", max_line, max_line_length);
+    return 0;
+}
+
+int get_line(char *line, int limit) {
     int i = 0;
     int c = 0;
-    while((c = getchar()) != EOF) {
+    while(((c = getchar()) != EOF) && i < limit) {
         if (c != '\n') {
             line[i] = c;
             ++i;
@@ -26,20 +45,4 @@ void copy_from_to(char * from, char * to) {
         ++i;
     }
     to[i] = '\0';
-}
-
-int main() {
-    char max_line[MAX_LINE_LENGTH];
-    char line[MAX_LINE_LENGTH];
-    int max_line_length = 0;
-    int line_length = 0;
-    while ((line_length = get_line(line)) > 0) {
-        // printf("Line = %s, length = %d\n", line, line_length);
-        if (line_length > max_line_length) {
-            copy_from_to(line, max_line);
-            max_line_length = line_length;
-        }
-    }
-    printf("Max line = %s, length = %d\n", max_line, max_line_length);
-    return 0;
 }
