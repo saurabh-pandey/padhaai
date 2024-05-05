69d906c9cc26308f3e34c838e36521192332e23e
Saurabh Pandey <saurabhpandey85@gmail.com>
2024-05-05 15:07:44 +0530

    PADHAAI: Working on implementing a shareable githook via this repo

--- .githooks/prepare-commit-msg.py
+++ .githooks/prepare-commit-msg.py
@@ -1,7 +1,36 @@
+import json
 import sys
+
+from pathlib import Path
+from subprocess import check_output
+from typing import Optional, List
+
+
+base_path = Path(__file__).parent
+
+
+def check_args() -> bool:
+    if len(sys.argv) < 3:
+        return False
+    if sys.argv[2] != "message":
+        return False
+    return True
+
+def is_disabled() -> bool:
+    config_file_path = (base_path / "../config.json").resolve()
+    with open(config_file_path) as config_f:
+        config = json.load(config_f)
+        if "enablePrepareCommitMsg" in config:
+            return False
+    return False
+
 
 def main():
     print(sys.argv)
+    if not check_args():
+        return 0
+    if is_disabled():
+        return 0
     return 0
 
 main()
