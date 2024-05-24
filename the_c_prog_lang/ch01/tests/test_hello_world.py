b04f60de63963df0720272355f6acbf3f45b201d
Saurabh Pandey <saurabhpandey85@gmail.com>
2024-05-24 22:17:45 +0530

    TCPL-CH01: Print to console seems to be working in the unittests

--- the_c_prog_lang/ch01/tests/test_hello_world.py
+++ the_c_prog_lang/ch01/tests/test_hello_world.py
@@ -1,4 +1,6 @@
 import subprocess
+# import logging
+# import sys
 
 inp1 = '''This is a long text. With multiple lines. Let's see what happens.
 This will be a very interesting way to drive C programs with getchar.
@@ -18,16 +20,22 @@ def
 
 import unittest
 
-import sys
-
 class TestHelloWorld(unittest.TestCase):
 
     def test_upper(self):
+        # log= logging.getLogger( "TestHelloWorld.test_upper" )
+        # run_output = subprocess.run(['./a.out'], capture_output=True, text=True, input=inp1).stdout
+        # log.debug("\n\nRun output = %s\n", run_output)
         print(subprocess.run(['./a.out'], capture_output=True, text=True, input=inp1).stdout)
 
     def test_isupper(self):
         print(subprocess.run(['./a.out'], capture_output=True, text=True, input=inp2).stdout)
+    
+    def test_empty(self):
+        print(subprocess.run(['./a.out'], capture_output=True, text=True, input="").stdout)
 
 
 if __name__ == '__main__':
+    # logging.basicConfig( stream=sys.stderr )
+    # logging.getLogger( "SomeTest.testSomething" ).setLevel(logging.DEBUG )
     unittest.main()
