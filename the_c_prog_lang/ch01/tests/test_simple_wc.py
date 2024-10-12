ff7a900ca2ab988419ed7ac751cfbc7d348aa4e5
Saurabh Pandey <saurabhpandey85@gmail.com>
2024-10-12 15:20:05 +0530

    TCPL-CH01: Added test for simple wc command clone

---
+++ the_c_prog_lang/ch01/tests/test_simple_wc.py
@@ -0,0 +1,54 @@
+import pathlib
+import subprocess
+import unittest
+
+
+single_inp = '''a
+bc
+def
+ghij
+klmnop
+'''
+
+empty_line_inp = '''
+'''
+
+multi_lines_inp = '''This is a multiline input. What purpose does it serve?
+It just tests the code below. I think this is the 3rd test.
+Today is 22/09/2024 and time is 13:56. It is a Sunday.
+'''
+
+class TestLongestLine(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.exe = "bin/" + pathlib.Path(__file__).stem[len("test_"):]
+
+    def test_simple(self):
+        output = subprocess.run([self.exe], capture_output=True, text=True, input=single_inp).stdout
+        self.assertEqual(output.splitlines(),
+                         ['Number of characters = 21, words = 5 and lines = 5'])
+    
+    def test_empty(self):
+        output = subprocess.run([self.exe], capture_output=True, text=True, input="").stdout
+        self.assertEqual(output.splitlines(),
+                         ['Number of characters = 0, words = 0 and lines = 0'])
+    
+    def test_empty_line(self):
+        output = subprocess.run([self.exe],
+                                capture_output=True,
+                                text=True,
+                                input=empty_line_inp).stdout
+        self.assertEqual(output.splitlines(),
+                         ['Number of characters = 1, words = 0 and lines = 1'])
+    
+    def test_multi_lines(self):
+        output = subprocess.run([self.exe],
+                                capture_output=True,
+                                text=True,
+                                input=multi_lines_inp).stdout
+        self.assertEqual(output.splitlines(),
+                         ['Number of characters = 170, words = 34 and lines = 3'])
+
+
+if __name__ == '__main__':
+    unittest.main()
