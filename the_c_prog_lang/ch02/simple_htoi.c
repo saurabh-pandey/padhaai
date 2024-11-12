38d7de793e13985b4367ae20e9e53f40d9ee934c
Saurabh Pandey <saurabhpandey85@gmail.com>
2024-11-12 20:18:52 +0530

    TCPL-CH02: Implemented simple hex to integer

--- the_c_prog_lang/ch02/simple_htoi.c
+++ the_c_prog_lang/ch02/simple_htoi.c
@@ -2,10 +2,33 @@
 #include <stdbool.h>
 
 int simple_htoi(char input[]) {
-    // for (int i = 0; input[i] != '\0'; ++i) {
+    // First 2 char of hex should be 0x or 0X
+    if (input[0] != '0') {
+        return -1;
+    }
+    if (input[1] != 'x' && input[1] != 'X') {
+        return -1;
+    }
+    // Check we have reached the end here so the input is just "0x" or "0X"
+    if (input[2] == '\0') {
+        return -1;
+    }
 
-    // }
-    return -1;
+    int n = 0;
+    for (int i = 2; input[i] != '\0'; ++i) {
+        int digit_value = -1;
+        if (input[i] >= '0' && input[i] <= '9') {
+            digit_value = input[i] - '0';
+        } else if (input[i] >= 'A' && input[i] <= 'F') {
+            digit_value = 10 + input[i] - 'A';
+        } else if (input[i] >= 'a' && input[i] <= 'f') {
+            digit_value = 10 + input[i] - 'a';
+        } else {
+            return -1;
+        }
+        n = n * 16 + digit_value;
+    }
+    return n;
 }
 
 
@@ -33,6 +56,10 @@ int main() {
         {"0xf", 15},
         {"1", -1},
         {"1f", -1},
+        {"0x", -1},
+        {"0X", -1},
+        {"0xg", -1},
+        {"0Xh", -1},
         {"", -1},
         {"a", -1},
         {"123a", -1},
@@ -52,7 +79,7 @@ int main() {
                    result);
         }
         if (result != arr[i].value) {
-            printf("FAILED input = %s, expected = %d and result1 = %d\n",
+            printf("FAILED input = %s, expected = %d and result = %d\n",
                    arr[i].input,
                    arr[i].value,
                    result);
