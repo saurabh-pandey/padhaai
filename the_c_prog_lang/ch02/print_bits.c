bea955ddeb87f3e2ce1f664521d29fa9e3e9bb3c
Saurabh Pandey <saurabhpandey85@gmail.com>
2024-11-15 22:18:28 +0530

    TCPL-CH02: Improved the printing of bits

--- the_c_prog_lang/ch02/print_bits.c
+++ the_c_prog_lang/ch02/print_bits.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
+
+const unsigned int size = sizeof(int) * 8;
+
 void fill_bits(char bits[], int n) {
-    unsigned int size = sizeof(int);
     int i = 0;
-    for(i = 0; i < size * 8; ++i) {
+    for(i = 0; i < size; ++i) {
         bits[i] = 0;
     }
     bits[i] = '\0';
 
-    for (i = 0; i < size * 8; ++i) {
-        const int bit = n & (1 << ((size * 8) - i - 1));
+    for (i = 0; i < size; ++i) {
+        const int bit = n & (1 << (size - i - 1));
         bits[i] = bit ? '1' : '0';
     }
 }
 
 int main() {
-    char bits[sizeof(int) * 16];
-    for (int n = -(1 << 4); n < (1 << 4); ++n) {
+    char bits[size + 1];
+    const int num_bits_to_use = 4;
+    for (int n = -(1 << num_bits_to_use); n < (1 << num_bits_to_use); ++n) {
         fill_bits(bits, n);
         printf("%d: %s\n", n, bits);
     }
