df483f5ccf0f5369df05973264e9d4110a55c780
Saurabh Pandey <saurabhpandey85@gmail.com>
2024-11-17 14:08:22 +0530

    TCPL-CH02: Done with printing bits of integer

--- the_c_prog_lang/ch02/print_bits.c
+++ the_c_prog_lang/ch02/print_bits.c
@@ -18,28 +18,44 @@ void fill_bits(char bits[], int n) {
 
 int binary_to_dec(char bits[]) {
     int n = 0;
-    int sign_bit = (bits[0] == '1') ? 1 : 0;
-    int sign_value = -1 * sign_bit;
-    // printf("  %d, %d\n", sign_bit, sign_value);
+    int sign_value = 1;
     for (int i = 1; bits[i] != '\0'; ++i) {
         int value = (bits[i] == '1') ? 1 : 0;
         n = value + n * 2;
-        sign_value *= 2;
-        // printf("  %d, %d\n", sign_bit, sign_value);
+        sign_value <<= 1;
     }
-    // printf("  %d, %d\n", sign_value, n);
+    int sign_bit = (bits[0] == '1') ? 1 : 0;
+    sign_value = -sign_bit * sign_value;
     return sign_value + n;
 
 }
 
 int main() {
     char bits[size + 1];
-    const int num_bits_to_use = 4;
+    const int num_bits_to_use = 15;
+    
+    printf("Running tests for print_bits\n");
+    
+    int debug = 0;
+    unsigned int num_failed = 0;
     for (int n = -(1 << num_bits_to_use); n < (1 << num_bits_to_use); ++n) {
         fill_bits(bits, n);
-        printf("%d: %s\n", n, bits);
         int res = binary_to_dec(bits);
-        printf("%s: %d\n\n", bits, res);
+        if (debug != 0) {
+            printf("n = %5d, bits = %s, converted value = %5d\n", n, bits, res);
+        }
+        if (res != n) {
+            printf("FAILED for n = %5d, bits = %s and result = %5d\n", n, bits, res);
+            ++num_failed;
+        }
     }
+
+    if (num_failed > 0) {
+        printf("%d test failed\n", num_failed);
+    } else {
+        printf("All tests passed\n");
+    }
+    printf("Done\n");
+    
     return 0;
 }
