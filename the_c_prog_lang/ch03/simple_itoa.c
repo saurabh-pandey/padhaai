2e373d53b06eeb2a654ad302594fae9f4129943a
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-02-03 20:48:12 +0530

    TCPL-CH03: Working on simple itoa implementation

---
+++ the_c_prog_lang/ch03/simple_itoa.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+void simple_itoa(int num, char output[]) {
+    // printf("%d %% %d = %d\n", 11, 10, 11 % 10);
+    // printf("%d %% %d = %d\n", 10, 10, 10 % 10);
+    // printf("%d %% %d = %d\n", -11, 10, -11 % 10);
+    // printf("%d %% %d = %d\n", -55, 10, -55 % 10);
+    const int sign = num < 0 ? -1 : 1;
+    int i = 0;
+    do {
+        const int digit = sign * (num % 10);
+        output[i] = '0' + digit;
+        ++i;
+        num /= 10;
+    } while (num != 0);
+    
+    // Add sign
+    if (sign == -1) {
+        output[i] = '-';
+        ++i;
+    }
+    
+    // reverse here
+    for (int start = 0, end = i - 1; start < end; ++start, --end) {
+        char temp = output[start];
+        output[start] = output[end];
+        output[end] = temp;
+    }
+    
+    output[i] = '\0';
+}
+
+typedef struct test_data {
+    int input;
+    char expected[12];
+} test_data;
+
+
+bool compare_strings(char str1[], char str2[]) {
+    int i = 0;
+    while(true) {
+        if (str1[i] != str2[i]) {
+            return false;
+        }
+        if (str1[i] == '\0') {
+            break;
+        }
+        ++i;
+    }
+    return true;
+}
+
+
+int main() {
+    bool debug = true;
+
+    // simple_itoa(0, "");
+    // return 0;
+    
+    printf("Running tests for simple_itoa\n");
+
+    test_data arr [] = {
+        {1, "1"},
+        {42, "42"},
+        {3, "3"},
+        {105, "105"},
+        {-1, "-1"},
+        {-10, "-10"},
+        {-42, "-42"},
+        {-123, "-123"},
+        {0, "0"},
+    };
+
+    unsigned int num_failed = 0;
+    for (int i = 0; i < sizeof(arr)/sizeof(test_data); ++i) {
+        char result[12] = {'\0'};
+        simple_itoa(arr[i].input, result);
+        if (debug) {
+            printf("Input = %d, expected = %s and result = %s\n",
+                   arr[i].input,
+                   arr[i].expected,
+                   result);
+        }
+        // if (!compare_strings(result, arr[i].expected)) {
+        //     printf("FAILED for input = %d, expected = %s and result1 = %s\n",
+        //            arr[i].input,
+        //            arr[i].expected,
+        //            result);
+        //     ++num_failed;
+        // }
+    }
+    if (num_failed > 0) {
+        printf("%d test failed\n", num_failed);
+    } else {
+        printf("All tests passed\n");
+    }
+    printf("Done\n");
+
+    return 0;
+}
