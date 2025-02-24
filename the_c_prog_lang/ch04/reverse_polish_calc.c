d10c94a8002c0ffac7f3da6ff0933b212c0d6b2e
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-02-24 21:16:03 +0530

    TCPL-CH04: Implemented some basic sketch of RPN calc

--- the_c_prog_lang/ch04/reverse_polish_calc.c
+++ the_c_prog_lang/ch04/reverse_polish_calc.c
@@ -1,15 +1,92 @@
 #include <stdio.h>
 
+#define MAX_STACK_SIZE 100
+
+
+int stack[MAX_STACK_SIZE];
+int stack_index = 0;
+
+void push(int num) {
+    if (stack_index == MAX_STACK_SIZE) {
+        printf("Error: Stack overflow\n");
+    }
+    stack[stack_index] = num;
+    ++stack_index;
+}
+
+int pop() {
+    if (stack_index == 0) {
+        printf("Error: Stack is empty\n");
+    }
+    --stack_index;
+    return stack[stack_index];
+}
+
 
 int main(int argc, char * const argv[]) {
-    if (argc < 2) {
-        printf("\nUsage:\n");
-        printf("   ./bin/reverse_polish_calc <some_calculation>\n");
-        printf("   Some examples are:\n");
-        printf("     ./bin/reverse_polish_calc 1 2 +\n");
-        printf("     ./bin/reverse_polish_calc 1 2 - 4 5 + *\n");
-        printf("\n");
-        return 0;
+    // if (argc < 2) {
+    //     printf("\nUsage:\n");
+    //     printf("   ./bin/reverse_polish_calc <some_calculation>\n");
+    //     printf("   Some examples are:\n");
+    //     printf("     ./bin/reverse_polish_calc 1 2 +\n");
+    //     printf("     ./bin/reverse_polish_calc 1 2 - 4 5 + *\n");
+    //     printf("\n");
+    //     return 0;
+    // }
+
+    int c = 0;
+    while ((c = getchar()) != EOF) {
+        if (c >= '0' && c <= '9') {
+            push(c - '0');
+        } else {
+            switch (c)
+            {
+                case '+':
+                {
+                    const int num2 = pop();
+                    const int num1 = pop();
+                    push(num1 + num2);
+                    break;
+                }
+                case '-':
+                {
+                    const int num2 = pop();
+                    const int num1 = pop();
+                    push(num1 - num2);
+                    break;
+                }
+                case '*':
+                {
+                    const int num2 = pop();
+                    const int num1 = pop();
+                    push(num1 * num2);
+                    break;
+                }
+                case '/':
+                {
+                    const int num2 = pop();
+                    if (num2 == 0) {
+                        printf("ERROR: Division by zero");
+                        return 1;
+                    }
+                    const int num1 = pop();
+                    push(num1 / num2);
+                    break;
+                }
+                case '\n':
+                {
+                    const int result = pop();
+                    // TODO: A check here that the stack is empty might be useful
+                    printf("Result = %d\n", result);
+                    break;
+                }
+                default:
+                {
+                    // Ignore everything else
+                    break;
+                }
+            }
+        }
     }
 
     return 0;
--- the_c_prog_lang/ch04/tests/test_reverse_polish_calc.py
+++ the_c_prog_lang/ch04/tests/test_reverse_polish_calc.py
@@ -9,17 +9,18 @@ class TestReversePolishCalc(unittest.TestCase):
         p = pathlib.Path(__file__)
         cls.exe = pathlib.Path(p.parents[1] / 'bin' / p.stem[len("test_"):])
 
-    def test_simple(self):
-        output = subprocess.run([self.exe, "1 2 +"],
-                                capture_output=True,
-                                text=True).stdout
-        print(output.splitlines())
+    # def test_simple(self):
+    #     output = subprocess.run([self.exe, "1 2 +"],
+    #                             capture_output=True,
+    #                             text=True).stdout
+    #     print(output.splitlines())
     
     def test_usage(self):
         output = subprocess.run([self.exe], capture_output=True, text=True).stdout
-        print(output.splitlines())
-        # self.assertEqual(output.splitlines(),
-        #                  ['','Usage:', '   ./bin/simple_pattern_search <pattern>', ''])
+        self.assertEqual(output.splitlines(),
+                         ['', 'Usage:', '   ./bin/reverse_polish_calc <some_calculation>',
+                          '   Some examples are:', '     ./bin/reverse_polish_calc 1 2 +',
+                          '     ./bin/reverse_polish_calc 1 2 - 4 5 + *', ''])
 
 
 if __name__ == '__main__':
