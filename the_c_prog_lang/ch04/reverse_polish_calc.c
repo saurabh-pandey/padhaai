7bc65146262ed59b58cec697983778db1ca912c7
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-02-26 11:59:11 +0530

    TCPL-CH04: Done with RPN expression evaluation

--- the_c_prog_lang/ch04/reverse_polish_calc.c
+++ the_c_prog_lang/ch04/reverse_polish_calc.c
@@ -1,83 +1,83 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 #define MAX_STACK_SIZE 100
 
 
-int stack[MAX_STACK_SIZE];
-int stack_index = 0;
+// Array backed Stack DS with push and pop
+double stack[MAX_STACK_SIZE];
+int stack_size = 0;
 
-void push(int num) {
-    if (stack_index == MAX_STACK_SIZE) {
-        printf("Error: Stack overflow\n");
+void push(double num) {
+    if (stack_size == MAX_STACK_SIZE) {
+        printf("ERROR: Max stack size reached so can't push to it.\n");
     }
-    stack[stack_index] = num;
-    ++stack_index;
+    stack[stack_size] = num;
+    ++stack_size;
 }
 
-int pop() {
-    if (stack_index == 0) {
-        printf("Error: Stack is empty\n");
+double pop() {
+    if (stack_size == 0) {
+        printf("ERROR: Stack is empty so can't pop\n");
     }
-    --stack_index;
-    return stack[stack_index];
+    --stack_size;
+    return stack[stack_size];
 }
 
 
+// RPN calculator
 int main(int argc, char * const argv[]) {
-    // if (argc < 2) {
-    //     printf("\nUsage:\n");
-    //     printf("   ./bin/reverse_polish_calc <some_calculation>\n");
-    //     printf("   Some examples are:\n");
-    //     printf("     ./bin/reverse_polish_calc 1 2 +\n");
-    //     printf("     ./bin/reverse_polish_calc 1 2 - 4 5 + *\n");
-    //     printf("\n");
-    //     return 0;
-    // }
+    if (argc < 2) {
+        printf("\nUsage:\n");
+        printf("   ./bin/reverse_polish_calc <some_calculation>\n");
+        printf("   Some examples are:\n");
+        printf("     ./bin/reverse_polish_calc 1 2 +\n");
+        printf("     ./bin/reverse_polish_calc 1 2 - 4 5 + \'*\'\n");
+        printf("\n");
+        return 0;
+    }
 
-    int c = 0;
-    while ((c = getchar()) != EOF) {
-        if (c >= '0' && c <= '9') {
-            push(c - '0');
+    // Skip the command itself so start from 1
+    for (int i = 1; i < argc; ++i) {
+        // Check if the first char is digit or -digit
+        if (isdigit(argv[i][0]) || (argv[i][0] == '-' && isdigit(argv[i][1]))) {
+            const double num = atof(argv[i]);
+            push(num);
         } else {
-            switch (c)
+            switch (argv[i][0])
             {
                 case '+':
                 {
-                    const int num2 = pop();
-                    const int num1 = pop();
+                    const double num2 = pop();
+                    const double num1 = pop();
                     push(num1 + num2);
                     break;
                 }
                 case '-':
                 {
-                    const int num2 = pop();
-                    const int num1 = pop();
+                    const double num2 = pop();
+                    const double num1 = pop();
                     push(num1 - num2);
                     break;
                 }
                 case '*':
                 {
-                    const int num2 = pop();
-                    const int num1 = pop();
+                    const double num2 = pop();
+                    const double num1 = pop();
                     push(num1 * num2);
                     break;
                 }
                 case '/':
                 {
-                    const int num2 = pop();
+                    const double num2 = pop();
                     if (num2 == 0) {
                         printf("ERROR: Division by zero");
                         return 1;
                     }
-                    const int num1 = pop();
+                    const double num1 = pop();
                     push(num1 / num2);
-                    break;
-                }
-                case '\n':
-                {
-                    const int result = pop();
-                    // TODO: A check here that the stack is empty might be useful
-                    printf("Result = %d\n", result);
                     break;
                 }
                 default:
@@ -88,6 +88,12 @@ int main(int argc, char * const argv[]) {
             }
         }
     }
+    const double result = pop();
+    if (stack_size > 0) {
+        printf("WARNING: Stack is not empty (size is %d) at the end of RPN expression evaluation\n",
+               stack_size);
+    }
+    printf("Result = %f\n", result);
 
     return 0;
 }
--- the_c_prog_lang/ch04/tests/test_reverse_polish_calc.py
+++ the_c_prog_lang/ch04/tests/test_reverse_polish_calc.py
@@ -8,19 +8,60 @@ class TestReversePolishCalc(unittest.TestCase):
     def setUpClass(cls):
         p = pathlib.Path(__file__)
         cls.exe = pathlib.Path(p.parents[1] / 'bin' / p.stem[len("test_"):])
-
-    # def test_simple(self):
-    #     output = subprocess.run([self.exe, "1 2 +"],
-    #                             capture_output=True,
-    #                             text=True).stdout
-    #     print(output.splitlines())
+    
+    def eval(self, expression: str) -> str:
+        return subprocess.run([self.exe, *expression.split()],
+                              capture_output=True,
+                              text=True).stdout
     
     def test_usage(self):
-        output = subprocess.run([self.exe], capture_output=True, text=True).stdout
+        output = self.eval("")
         self.assertEqual(output.splitlines(),
                          ['', 'Usage:', '   ./bin/reverse_polish_calc <some_calculation>',
                           '   Some examples are:', '     ./bin/reverse_polish_calc 1 2 +',
-                          '     ./bin/reverse_polish_calc 1 2 - 4 5 + *', ''])
+                          '     ./bin/reverse_polish_calc 1 2 - 4 5 + \'*\'', ''])
+    
+    def test_simple_int(self):
+        output = self.eval("1 2 +")
+        self.assertEqual(output.splitlines(), ['Result = 3.000000'])
+        output = self.eval("1 2 -")
+        self.assertEqual(output.splitlines(), ['Result = -1.000000'])
+        output = self.eval("3 2 *")
+        self.assertEqual(output.splitlines(), ['Result = 6.000000'])
+        output = self.eval("6 2 /")
+        self.assertEqual(output.splitlines(), ['Result = 3.000000'])
+    
+    def test_simple_double(self):
+        output = self.eval("1.1 2.2 +")
+        self.assertEqual(output.splitlines(), ['Result = 3.300000'])
+        output = self.eval("1.4 2.2 -")
+        self.assertEqual(output.splitlines(), ['Result = -0.800000'])
+        output = self.eval("3.1 2.7 *")
+        self.assertEqual(output.splitlines(), ['Result = 8.370000'])
+        output = self.eval("6.5 1.3 /")
+        self.assertEqual(output.splitlines(), ['Result = 5.000000'])
+    
+    def test_bigger(self):
+        output = self.eval("1 2 - 4 5 + *")
+        self.assertEqual(output.splitlines(), ['Result = -9.000000'])
+    
+    def test_incorrect_rpn(self):
+        output = self.eval("1 2 - 4")
+        self.assertEqual(output.splitlines(),
+                         ['WARNING: Stack is not empty (size is 1) at the end of RPN expression evaluation', 'Result = 4.000000'])
+    
+    def test_empty(self):
+        output = self.eval("+")
+        self.assertEqual(output.splitlines(),
+                         ["ERROR: Stack is empty so can't pop", 'Result = 0.000000'])
+    
+    def test_single(self):
+        output = self.eval("1")
+        self.assertEqual(output.splitlines(), ['Result = 1.000000'])
+    
+    def test_negative(self):
+        output = self.eval("-42")
+        self.assertEqual(output.splitlines(), ['Result = -42.000000'])
 
 
 if __name__ == '__main__':
