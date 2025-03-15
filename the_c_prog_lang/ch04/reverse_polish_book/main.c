4638f3f0561a762588018d4feea81291b87866ee
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-03-15 22:49:11 +0530

    TCPL-CH04: Working parser of rpn

--- the_c_prog_lang/ch04/reverse_polish_book/main.c
+++ the_c_prog_lang/ch04/reverse_polish_book/main.c
@@ -1,54 +1,96 @@
 #include <ctype.h>
 #include <stdio.h>
 
-#define MAX_INP 20
 
-int getop(char parsed_inp[]) {
-    int c = 0;
-    // Skip all whitespace char in the beginning
-    while((c = getchar()) != EOF) {
+#define MAX_INP 20
+#define MAX_BUFFER 20
+
+
+typedef enum {
+    NUMBER,
+    OP,
+    NEW_LINE,
+    END_OF_FILE,
+    INVALID
+} Token;
+
+
+char buffer[MAX_BUFFER];
+int buffPt = 0;
+
+
+Token getop(char parsed_inp[]) {
+    // printf("Buffer data = %c, pt = %d\n", buffer[0], buffPt);
+    int c = buffPt > 0 ? buffer[--buffPt] : getchar();
+    // printf("c = %c, val = %d, pt = %d\n", c, c, buffPt);
+    // Skip all whitespace char
+    while(1) {
+        // printf("1- %c\n", c);
         if (c == ' ' || c == '\t') {
+            c = getchar();
             continue;
         } else {
             break;
         }
     }
 
-    // c = getchar();
+    // printf("2- %c\n", c);
+
     if (c == EOF) {
-        return -1;
+        return END_OF_FILE;
     }
     
     // Parse for op
     parsed_inp[1] = '\0';
     if (c != '.' && !isdigit(c)) {
+        // printf("3- %c\n", c);
         parsed_inp[0] = c;
-        return 0;
+        return OP;
     }
     
     // Parse for number
     int i = 0;
     parsed_inp[i] = c;
+    // printf("4- %c\n", c);
     ++i;
-    while ((c = getchar()) != EOF) {
+    // while ((c = getchar()) != EOF) {
+    while (1) {
+        c = getchar();
+        // printf("5- %c\n", c);
         if (isdigit(c)) {
             parsed_inp[i] = c;
         } else if (c == '.') {
             parsed_inp[i] = c;
         } else {
-            break;
+            parsed_inp[i] = '\0';
+            buffer[buffPt++] = c;
+            // printf("6- %c\n", c);
+            return NUMBER; 
         }
         ++i;
     }
-    parsed_inp[i] = '\0';
+
+    // printf("7- %c\n", c);
+    
     return 0;
 }
 
 int main() {
     int ret_val = 0;
     char parsed_inp[MAX_INP] = "\0";
-    while((ret_val = getop(parsed_inp)) != -1) {
-        printf("<=> %s\n", parsed_inp);
+    while(1) {
+        ret_val = getop(parsed_inp);
+        switch (ret_val)
+        {
+            case NUMBER:
+            case OP:
+            case NEW_LINE:
+                printf("<=> %s\n", parsed_inp);
+                break;
+            default:
+                printf("<=> DONE %s\n", parsed_inp);
+                return 0;
+        }
     }
     return 0;
 }
