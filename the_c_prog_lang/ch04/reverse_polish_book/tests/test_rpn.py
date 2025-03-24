0cfb0cc382a53d3f2dabe47cb628a6621e817dee
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-03-24 22:45:26 +0530

    TCPL-CH04: Added some more test for RPN

--- the_c_prog_lang/ch04/reverse_polish_book/tests/test_rpn.py
+++ the_c_prog_lang/ch04/reverse_polish_book/tests/test_rpn.py
@@ -9,6 +9,15 @@ simple_input = '''2 3 +
 2 3 /
 '''
 
+floating_input = '''2.1 3.1 +
+5.2 3.1 -
+3.3 1.7 *
+2.1 3.7 /
+'''
+
+bigger_input = '''1 2 - 4 5 + *
+'''
+
 class TestRpnCalc(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -20,7 +29,32 @@ class TestRpnCalc(unittest.TestCase):
                                 capture_output=True,
                                 text=True,
                                 input=simple_input).stdout
-        print(output.splitlines())
+        # print(output.splitlines())
+        self.assertEqual(output.splitlines(), ['expr> Result = 5.000000',
+                                               'expr> Result = -1.000000',
+                                               'expr> Result = 6.000000',
+                                               'expr> Result = 0.666667',
+                                               'expr> DONE ', ''])
+    
+    def test_floating(self):
+        output = subprocess.run([self.exe],
+                                capture_output=True,
+                                text=True,
+                                input=floating_input).stdout
+        # print(output.splitlines())
+        self.assertEqual(output.splitlines(), ['expr> Result = 5.200000',
+                                               'expr> Result = 2.100000',
+                                               'expr> Result = 5.610000',
+                                               'expr> Result = 0.567568',
+                                               'expr> DONE ', ''])
+    
+    def test_bigger(self):
+        output = subprocess.run([self.exe],
+                                capture_output=True,
+                                text=True,
+                                input=bigger_input).stdout
+        # print(output.splitlines())
+        self.assertEqual(output.splitlines(), ['expr> Result = -9.000000', 'expr> DONE ', ''])
 
 if __name__ == '__main__':
     unittest.main()
