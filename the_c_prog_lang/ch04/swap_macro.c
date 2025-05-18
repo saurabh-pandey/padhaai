4f61c3b729334ea0c6d19561c969215cd7f40c66
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-05-18 16:27:25 +0530

    TCPL-CH04: Swap using macro definition for generic types

---
+++ the_c_prog_lang/ch04/swap_macro.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define MAX_NUM 100
+#define MIN_NUM -MAX_NUM
+
+
+#define SWAP(t, a, b) { \
+t temp; \
+temp = a; \
+a = b; \
+b = temp; \
+} \
+
+int main() {
+    int a = 2;
+    int b = 3;
+
+    printf("Before swap = (%d, %d)\n", a, b);
+    SWAP(int, a, b)
+    printf("After  swap = (%d, %d)\n", a, b);
+
+    double c = 2.1;
+    double d = 3.2;
+
+    printf("Before swap = (%f, %f)\n", c, d);
+    SWAP(double, c, d)
+    printf("After swap = (%f, %f)\n", c, d);
+
+    return 0;
+}
