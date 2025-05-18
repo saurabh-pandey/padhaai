c79295d2a960962b990e53ce2e2f3568b7fcd668
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-05-18 16:28:53 +0530

    TCPL-CH04: Better macro version that allows a semicolon at the end using do while idea

--- the_c_prog_lang/ch04/swap_macro.c
+++ the_c_prog_lang/ch04/swap_macro.c
@@ -4,26 +4,26 @@
 #define MIN_NUM -MAX_NUM
 
 
-#define SWAP(t, a, b) { \
+#define SWAP(t, a, b) do { \
 t temp; \
 temp = a; \
 a = b; \
 b = temp; \
-} \
+} while(0) \
 
 int main() {
     int a = 2;
     int b = 3;
 
     printf("Before swap = (%d, %d)\n", a, b);
-    SWAP(int, a, b)
+    SWAP(int, a, b);
     printf("After  swap = (%d, %d)\n", a, b);
 
     double c = 2.1;
     double d = 3.2;
 
     printf("Before swap = (%f, %f)\n", c, d);
-    SWAP(double, c, d)
+    SWAP(double, c, d);
     printf("After swap = (%f, %f)\n", c, d);
 
     return 0;
