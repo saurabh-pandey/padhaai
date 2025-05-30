012f19a7222b56e5317142d472d2554b6408c943
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-05-30 23:12:23 +0530

    TCPL-CH05: Also adding some stuff for strncat

--- the_c_prog_lang/ch05/simple_str_n.c
+++ the_c_prog_lang/ch05/simple_str_n.c
@@ -12,19 +12,6 @@ void my_strncpy(char *s, char *t, int n) {
     }
 }
 
-void my_strncat(char *s, char *t) {
-    while (*s != '\0') {
-        ++s;
-    }
-
-    while (*t != '\0') {
-        *s = *t;
-        ++s;
-        ++t;
-    }
-    *s = '\0';
-}
-
 int my_strncmp(char *s, char *t) {
     while (*s == *t) {
         if (*s == '\0') {
@@ -43,11 +30,10 @@ typedef struct strncpy_test_data {
     char *expected;
 } strncpy_test_data;
 
-
-int main() {
+void run_strncpy_tests() {
     bool debug = false;
     
-    printf("Running tests for simple_str_n\n");
+    printf("Running tests for simple_strncpy\n");
 
     strncpy_test_data strncpy_tests[] = {
         {"abc", 1, "a"},
@@ -65,10 +51,10 @@ int main() {
     unsigned int num_failed = 0;
     for (int i = 0; i < sizeof(strncpy_tests)/sizeof(strncpy_test_data); ++i) {
         char t[MAX_STR_LEN] = "";
-        my_strcpy(t, strncpy_tests[i].s, strncpy_tests[i].n);
+        my_strncpy(t, strncpy_tests[i].s, strncpy_tests[i].n);
 
         if (debug) {
-            printf("s = %s, n = %d, t = %s, expected\n",
+            printf("s = %s, n = %d, t = %s, expected = %s\n",
                    strncpy_tests[i].s,
                    strncpy_tests[i].n,
                    t,
@@ -85,6 +71,72 @@ int main() {
         printf("All tests passed\n");
     }
     printf("Done\n");
+}
 
+
+void my_strncat(char *s, char *t, int n) {
+    while (*s != '\0') {
+        ++s;
+    }
+
+    while (*t != '\0') {
+        *s = *t;
+        ++s;
+        ++t;
+    }
+    *s = '\0';
+}
+
+
+typedef struct strncat_test_data {
+    char s[MAX_STR_LEN];
+    char t[SMALL_STR_LEN];
+    int n;
+    char *expected;
+} strncat_test_data;
+
+
+void run_strncat_tests() {
+    bool debug = false;
+    
+    printf("Running tests for simple_strncat\n");
+
+    strncat_test_data strncat_tests[] = {
+        {"abc", "ba", 2, "abcba"},
+        {"abc", "ba", 1, "abcb"},
+        {"abc", "ba", 0, "abc"},
+        {"abc", "ba", -1, "abc"},
+        {"abc", "ba", 3, "abcba"},
+        {"abc", "ba", 4, "abcba"},
+    };
+
+    unsigned int num_failed = 0;
+    for (int i = 0; i < sizeof(strncat_tests)/sizeof(strncat_test_data); ++i) {
+        my_strncat(strncat_tests[i].s, strncat_tests[i].t, strncat_tests[i].n);
+
+        if (debug) {
+            printf("s = %s, n = %d, t = %s, expected = %s\n",
+                   strncat_tests[i].s,
+                   strncat_tests[i].n,
+                   strncat_tests[i].t,
+                   strncat_tests[i].expected);
+        }
+        const int cmp_res = my_strncmp(strncat_tests[i].s, strncat_tests[i].expected);
+        if (cmp_res != 0) {
+            ++num_failed;
+        }
+    }
+    if (num_failed > 0) {
+        printf("%d test failed\n", num_failed);
+    } else {
+        printf("All tests passed\n");
+    }
+    printf("Done\n");
+}
+
+
+int main() {
+    run_strncpy_tests();
+    run_strncat_tests();
     return 0;
 }
