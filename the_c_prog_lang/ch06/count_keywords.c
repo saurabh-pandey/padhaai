2c1fa1f508e83fb533732e5c217cfb4caa3a94bf
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-06-19 21:14:16 +0530

    TCPL-CH06: Starting to implement counting keywords

--- the_c_prog_lang/ch06/count_keywords.c
+++ the_c_prog_lang/ch06/count_keywords.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define MAX_WORD_LEN 100
+
+
 // Alphabetically sorted keywords so that we can do binary search. Just added some of the keywords
 struct keyword {
     char *word;
@@ -24,7 +27,31 @@ struct keyword {
     {"void", 0},
 };
 
+
+int getword(char *word) {
+    return 0;
+}
+
+struct keyword * binsearch(struct keyword *left, struct keyword *right, char *word) {
+    return NULL;
+}
+
+
 int main(int argc, char *argv[]) {
     printf("Count all keyword occurrences\n");
+    char word[MAX_WORD_LEN] = "";
+
+    const int keywords_size = sizeof(keywords) / sizeof(keywords[0]);
+    while (getword(word) != EOF) {
+        struct keyword *word_found = NULL;
+        if ((word_found = binsearch(&keywords[0], &keywords[keywords_size - 1], word)) != NULL) {
+            (word_found->count)++;
+        }
+    }
+    
+    for (int i = 0; i < keywords_size; ++i) {
+        printf("%s -> %d\n", keywords[i].word, keywords[i].count);
+    }
+
     return 0;
 }
