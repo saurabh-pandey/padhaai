5144688aefdf999f2476233fa35c935ed7f03589
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-06-30 22:02:23 +0530

    TCPL-CH06: Added a rough sketch of the binary tree used in word count

--- the_c_prog_lang/ch06/word_count.c
+++ the_c_prog_lang/ch06/word_count.c
@@ -21,13 +21,54 @@ int getword(char *word) {
 }
 
 
+struct node {
+    char *word;
+    int count;
+    struct node *left;
+    struct node *right;
+};
+
+struct node * create_node(char *word) {
+    return NULL;
+}
+
+struct node * add_node(char *word, struct node *root) {
+    if (word == NULL) {
+        // TODO: Should be a warning?
+        return NULL;
+    }
+    
+    if (root == NULL) {
+        return create_node(word);
+    } else {
+        const int res = strcmp(root->word, word);
+        if (res == 0) {
+            ++(root->count);
+        } else if (res < 0) {
+            return add_node(word, root->left);
+        } else {
+            return add_node(word, root->left);
+        }
+    }
+    
+    return NULL;
+}
+
+
+void tree_print(struct node *root) {}
+
+
 int main(int argc, char *argv[]) {
     printf("Count all words\n");
     char word[MAX_WORD_LEN] = "";
     int word_len = 0;
+    struct node *root = NULL;
     while ((word_len = getword(word)) != EOF) {
+        root = add_node(word, root);
         printf("%s\n", word);
     }
+
+    tree_print(root);
 
     return 0;
 }
