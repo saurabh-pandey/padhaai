6d7da877dd5ed3e61b366f0bcbae21609518a753
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-08-03 13:26:50 +0530

    TCPL-CH07: Added a simple test to run scanf experiments

---
+++ the_c_prog_lang/ch07/tests/test_try_scanf.py
@@ -0,0 +1,27 @@
+import pathlib
+import subprocess
+import unittest
+import textwrap
+
+
+class TestTryScanf(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        p = pathlib.Path(__file__)
+        cls.exe = pathlib.Path(p.parents[1] / 'bin' / p.stem[len("test_"):])
+    
+    def test_simple(self):
+        arr_inp = textwrap.dedent('''\
+            412
+            3.141
+            helloworld
+        ''')
+        output = subprocess.run([self.exe],
+                                capture_output=True,
+                                text=True,
+                                input=arr_inp).stdout
+        print(output.splitlines())
+        # self.assertEqual(output.splitlines(), ['arr = [1, 2, 3, 4, 5, 0, 0, 0, 0, 0]'])
+
+if __name__ == '__main__':
+    unittest.main()
