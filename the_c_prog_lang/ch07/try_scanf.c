d9763f484b833a5564184dc7333b9f9ed5957951
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-08-04 21:45:26 +0530

    TCPL-CH07: Some experiments with scanf

--- the_c_prog_lang/ch07/try_scanf.c
+++ the_c_prog_lang/ch07/try_scanf.c
@@ -24,12 +24,53 @@ void try_string(void) {
     printf("String entered is = %s\n", str);
 }
 
+void test_int_tuples(void) {
+    int num1 = 0;
+    int num2 = 0;
+    int num3 = 0;
+    printf("Enter three numbers like (n1, n2, n3): ");
+
+    //NOTE: The space before '(' in below scanf is to ignore any newline character in the buffer!!
+    scanf(" (%d, %d, %d)", &num1, &num2, &num3);
+    printf("Three numbers are (%d, %d, %d)\n", num1, num2, num3);
+}
+
+void test_date_fmt1(void) {
+    int date = 0;
+    char month[20];
+    int year = 0;
+
+    printf("Enter date in dd-month_name_yy format (e.g. 02-March-1978): ");
+    // scanf below won't work as string width is going to be the size of month (minus one for null 
+    // char). Suppose I enter 18-March-2024 then the month array will be "March-2024" and year will
+    // be 0.
+    // scanf("%d-%s-%d", &date, month, &year);
+    scanf("%d-%19[^-]-%d", &date, month, &year);
+    printf("Entered date is %d-%s-%d\n", date, month, year);
+}
+
+void test_date_fmt2(void) {
+    int date = 0;
+    int month = 0;
+    int year = 0;
+
+    printf("Enter date in dd/mm/yyyy format (e.g. 02/03/1978): ");
+    scanf("%d/%d/%d", &date, &month, &year);
+    printf("Entered date is %d/%d/%d\n", date, month, year);
+}
+
 int main() {
     printf("Running tests for try_scanf\n");
 
     try_int();
     try_double();
     try_string();
+
+    test_int_tuples();
+
+    test_date_fmt1();
+
+    test_date_fmt2();
 
     printf("Done\n");
     
