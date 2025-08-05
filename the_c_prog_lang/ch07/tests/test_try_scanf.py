2eee102ac09c80c110fc1f5ea3aaff5804da847c
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-08-05 18:15:47 +0530

    TCPL-CH07: Done with scanf tests

--- the_c_prog_lang/ch07/tests/test_try_scanf.py
+++ the_c_prog_lang/ch07/tests/test_try_scanf.py
@@ -23,8 +23,42 @@ class TestTryScanf(unittest.TestCase):
                                 capture_output=True,
                                 text=True,
                                 input=arr_inp).stdout
-        print(output.splitlines())
-        # self.assertEqual(output.splitlines(), ['arr = [1, 2, 3, 4, 5, 0, 0, 0, 0, 0]'])
+        self.assertEqual(output.splitlines(),
+                         ['Running tests for try_scanf',
+                          'Enter an int: int entered is = 412',
+                          'Enter a double: double entered is = 3.141000',
+                          'Enter a string: String entered is = helloworld',
+                          'Enter three numbers like (n1, n2, n3): Three numbers are (-17, 5, 43)', 
+                          'Enter date in dd-month_name_yy format (e.g. 02-March-1978): Entered date'
+                          ' is 3-Nov-1897',
+                          'Enter date in dd/mm/yyyy format (e.g. 02/03/1978): Entered date is'
+                          ' 3/1/2021',
+                          'Done'])
+    
+    def test_another(self):
+        arr_inp = textwrap.dedent('''\
+            -412
+            -3.141
+            def
+            (3, -9, -101)
+            13-October-2023
+            23/12/2001
+        ''')
+        output = subprocess.run([self.exe],
+                                capture_output=True,
+                                text=True,
+                                input=arr_inp).stdout
+        self.assertEqual(output.splitlines(),
+                         ['Running tests for try_scanf',
+                          'Enter an int: int entered is = -412',
+                          'Enter a double: double entered is = -3.141000',
+                          'Enter a string: String entered is = def',
+                          'Enter three numbers like (n1, n2, n3): Three numbers are (3, -9, -101)', 
+                          'Enter date in dd-month_name_yy format (e.g. 02-March-1978): Entered date'
+                          ' is 13-October-2023',
+                          'Enter date in dd/mm/yyyy format (e.g. 02/03/1978): Entered date is'
+                          ' 23/12/2001',
+                          'Done'])
 
 if __name__ == '__main__':
     unittest.main()
