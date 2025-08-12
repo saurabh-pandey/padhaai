deeb1a7fbed315e85c313793c0587f59fbcefca7
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-08-12 19:31:51 +0530

    TCPL-CH07: Trying to check the path detection mechanism

--- the_c_prog_lang/ch07/simple_cat.c
+++ the_c_prog_lang/ch07/simple_cat.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <linux/limits.h>
+
 
 int main(int argc, char *argv[]) {
     printf("Running simple_cat\n");
+
+    char cwd[PATH_MAX];
+   if (getcwd(cwd, sizeof(cwd)) != NULL) {
+       printf("Current working dir: %s\n", cwd);
+   } else {
+       perror("getcwd() error");
+       return 1;
+   }
+
+   char exec_path[PATH_MAX];
+   ssize_t len = readlink("/proc/self/exe", exec_path, sizeof(exec_path) - 1);
+    if (len == -1) {
+        perror("Error reading symbolic link");
+        return 1;
+    }
+
+    // Null-terminate the path
+    exec_path[len] = '\0';
+
+    printf("Path to exec is '%s'\n", exec_path);
+
 
     FILE *fp;
     if ((fp = fopen("tests/data/text1.txt", "r")) != NULL) {
