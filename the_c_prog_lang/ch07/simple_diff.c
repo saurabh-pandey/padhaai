01ab736aaaf3d3ae52db1f79e192586bad9b41c6
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-08-15 23:22:51 +0530

    TCPL-CH07: Reading lines for diff of two files

--- the_c_prog_lang/ch07/simple_diff.c
+++ the_c_prog_lang/ch07/simple_diff.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <linux/limits.h>
 
@@ -10,18 +11,43 @@ int main(int argc, char *argv[]) {
         FILE *file1 = fopen(argv[1], "r");
         if (file1 == NULL) {
             fprintf(stderr, "ERROR: %s Can't open file %s\n", argv[0], argv[1]);
+            exit(1);
         }
         FILE *file2 = fopen(argv[2], "r");
         if (file2 == NULL) {
             fprintf(stderr, "ERROR: %s Can't open file %s\n", argv[0], argv[2]);
+            exit(1);
         }
         
-        while (0) {
+        while (1) {
             // read line in each file
+            char line1[100];
+            int is_file1_eof = 0;
+            if ((is_file1_eof == 0) && (fgets(line1, 100, file1) != NULL)) {
+                printf("Line1: %s\n", line1);
+            } else {
+                is_file1_eof = 1;
+            }
+
+            char line2[100];
+            int is_file2_eof = 0;
+            if ((is_file2_eof == 0) && (fgets(line2, 100, file2) != NULL)) {
+                printf("Line2: %s\n", line2);
+            } else {
+                is_file2_eof = 1;
+            }
+
             // compare
             // if diff print
-            // break if EOF of both files reached
+
+            // Break if both files are EOF
+            if (is_file1_eof && is_file2_eof) {
+                break;
+            }
         }
+        
+        fclose(file1);
+        fclose(file2);
         
     }
     return 0;
