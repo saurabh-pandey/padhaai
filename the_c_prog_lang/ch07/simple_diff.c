c50098e1cd7dddfb0985aa090fe77157e58bb844
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-08-16 13:03:37 +0530

    TCPL-CH07: Working version of simple diff

--- the_c_prog_lang/ch07/simple_diff.c
+++ the_c_prog_lang/ch07/simple_diff.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <linux/limits.h>
 
@@ -19,32 +20,56 @@ int main(int argc, char *argv[]) {
             exit(1);
         }
         
+        int nums_match = 0;
+        int nums_mismatch = 0;
+        int nums_one_sided = 0;
+        int nums_file1_lines = 0;
+        int nums_file2_lines = 0;
         while (1) {
             // read line in each file
-            char line1[100];
+            char line1[150];
             int is_file1_eof = 0;
-            if ((is_file1_eof == 0) && (fgets(line1, 100, file1) != NULL)) {
+            if ((is_file1_eof == 0) && (fgets(line1, 150, file1) != NULL)) {
                 printf("Line1: %s\n", line1);
+                ++nums_file1_lines;
             } else {
                 is_file1_eof = 1;
             }
 
-            char line2[100];
+            char line2[150];
             int is_file2_eof = 0;
-            if ((is_file2_eof == 0) && (fgets(line2, 100, file2) != NULL)) {
+            if ((is_file2_eof == 0) && (fgets(line2, 150, file2) != NULL)) {
                 printf("Line2: %s\n", line2);
+                ++nums_file2_lines;
             } else {
                 is_file2_eof = 1;
             }
 
             // compare
-            // if diff print
-
-            // Break if both files are EOF
-            if (is_file1_eof && is_file2_eof) {
+            if (is_file1_eof == 0 && is_file2_eof == 0) {
+                // Compare if 
+                if (strcmp(line1, line2) == 0) {
+                   printf("  Lines match\n");
+                   ++nums_match;
+                } else {
+                    printf("  Lines don't match due to strcmp\n");
+                    ++nums_mismatch;
+                }
+            } else if ((is_file1_eof == 1) && (is_file2_eof == 1)) {
+                // Break if both files are EOF
                 break;
+            } else {
+                printf("  Lines don't match because EOF\n");
+                ++nums_one_sided;
             }
         }
+
+        printf("Some overall statistics:\n");
+        printf("No. of lines matching      = %d\n", nums_match);
+        printf("No. of lines with diff     = %d\n", nums_mismatch);
+        printf("No. of lines only one file = %d\n", nums_one_sided);
+        printf("No. of lines file1         = %d\n", nums_file1_lines);
+        printf("No. of lines file2         = %d\n", nums_file2_lines);
         
         fclose(file1);
         fclose(file2);
