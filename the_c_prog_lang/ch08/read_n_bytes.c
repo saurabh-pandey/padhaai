c20cb12397fdf6666d29cd99ec925694fa9e5960
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-08-26 23:24:06 +0530

    TCPL-CH08: Some more fun with lseek

--- the_c_prog_lang/ch08/read_n_bytes.c
+++ the_c_prog_lang/ch08/read_n_bytes.c
@@ -10,6 +10,36 @@
 //     return 0;
 // }
 
+ssize_t seek_opened_file(int fd, off_t offset, int nbytes) {
+    printf("Seeking fd = %d at offset = %ld for bytes = %d\n", fd, offset, nbytes);
+    // off_t ret = lseek(fd, 45, SEEK_CUR);
+    off_t ret = lseek(fd, offset, SEEK_DATA);
+
+    printf("ret = %ld\n", ret);
+
+    if (ret == -1) {
+        char error_msg[100];
+        snprintf(error_msg, sizeof(error_msg), "Error while seeking fd %d", fd);
+        perror(error_msg);
+        exit(1);
+    }
+
+    char buffer[100];
+    const ssize_t nread = read(fd, buffer, nbytes);
+    printf("nread = %ld\n", nread);
+    if (nread == -1) {
+        char error_msg[100];
+        snprintf(error_msg, sizeof(error_msg), "Error while reading fd %d", fd);
+        perror(error_msg);
+        exit(1);
+    }
+
+    write(1, buffer, nread);
+    write(1, "\n", 1);
+    return nread;
+}
+
+
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -24,28 +54,17 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    // off_t ret = lseek(fd, 45, SEEK_CUR);
-    off_t ret = lseek(fd, 50, SEEK_DATA);
+    seek_opened_file(fd, 0, 10);
+    seek_opened_file(fd, 5, 10);
+    seek_opened_file(fd, 15, 10);
+    seek_opened_file(fd, 20, 10);
+    seek_opened_file(fd, 32, 10);
+    seek_opened_file(fd, 34, 10);
+    seek_opened_file(fd, 35, 10);
+    seek_opened_file(fd, 36, 10);
+    seek_opened_file(fd, 37, 10);
 
-    printf("ret = %ld\n", ret);
-
-    if (ret == -1) {
-        char error_msg[100];
-        snprintf(error_msg, sizeof(error_msg), "Error while lseeking file %s", argv[1]);
-        perror(error_msg);
-        exit(1);
-    }
-
-    char buffer[100];
-    ssize_t nread = read(fd, buffer, 10);
-    if (nread == -1) {
-        char error_msg[100];
-        snprintf(error_msg, sizeof(error_msg), "Error while reading file %s", argv[1]);
-        perror(error_msg);
-        exit(1);
-    }
-
-    write(1, buffer, nread);
+    close(fd);
 
     return 0;
 }
