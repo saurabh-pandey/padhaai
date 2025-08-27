33dbcc0623f8ab2b69e9bb5e9570d1fe51988281
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-08-27 14:34:03 +0530

    TCPL-CH08: Done with experimenting lseek

--- the_c_prog_lang/ch08/lseek_trials.c
+++ the_c_prog_lang/ch08/lseek_trials.c
@@ -7,36 +7,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 #define MAX_BUFFER_SIZE 100
+
 
 // This function uses SEEK_SET to seek into a file
 // SEEK_SET doesn't fail even when we seek beyond the EOF. It is only later that a call to read
 // that returns 0 confirms EOF. Thus it is an approach where we seek and read until we detect EOF.
 ssize_t seek_set_opened_file(int fd, off_t offset, char * buffer, int nbytes) {
-    // printf("Seeking using SEEK_SET fd = %d at offset = %ld for bytes = %d\n", fd, offset, nbytes);
-    off_t ret = lseek(fd, offset, SEEK_SET);
-
-    // printf("  lseek ret = %ld\n", ret);
-
+    // Seek using SEEK_SET for seeking from file start
+    const off_t ret = lseek(fd, offset, SEEK_SET);
     if (ret == -1) {
-        char error_msg[100];
+        char error_msg[MAX_BUFFER_SIZE];
         snprintf(error_msg, sizeof(error_msg), "Error while seeking fd %d", fd);
         perror(error_msg);
         return -1;
     }
 
+    // Read now nbytes starting from offset into the buffer
     const ssize_t nread = read(fd, buffer, nbytes);
-    // printf("  nread = %ld\n", nread);
     if (nread == -1) {
-        char error_msg[100];
+        char error_msg[MAX_BUFFER_SIZE];
         snprintf(error_msg, sizeof(error_msg), "Error while reading fd %d", fd);
         perror(error_msg);
         return -1;
     }
 
-    // printf("  Output:\n");
-    // write(1, buffer, nread);
-    // write(1, "\n", 2);
     return nread;
 }
 
@@ -45,35 +41,29 @@ ssize_t seek_set_opened_file(int fd, off_t offset, char * buffer, int nbytes) {
 // SEEK_DATA fails i.e returns -1 when we seek beyond the EOF. Thus we don't need a read call to
 // detect EOF.
 ssize_t seek_data_opened_file(int fd, off_t offset, char * buffer, int nbytes) {
-    // printf("Seeking using SEEK_DATA fd = %d at offset = %ld for bytes = %d\n", fd, offset, nbytes);
-    off_t ret = lseek(fd, offset, SEEK_DATA);
-
-    // printf("  lseek ret = %ld\n", ret);
-
+    // Seek using SEEK_DATA and error if go beyond EOF
+    const off_t ret = lseek(fd, offset, SEEK_DATA);
     if (ret == -1) {
-        // char error_msg[100];
-        // snprintf(error_msg, sizeof(error_msg), "Error while seeking fd %d", fd);
-        // perror(error_msg);
+        // EOF so return 0 bytes read
         return 0;
     }
 
+    // Read now nbytes starting from offset into the buffer
     const ssize_t nread = read(fd, buffer, nbytes);
-    // printf("  nread = %ld\n", nread);
     if (nread == -1) {
-        char error_msg[100];
+        char error_msg[MAX_BUFFER_SIZE];
         snprintf(error_msg, sizeof(error_msg), "Error while reading fd %d", fd);
         perror(error_msg);
         return -1;
     }
 
-    // printf("  Output:\n");
-    // write(1, buffer, nread);
-    // write(1, "\n", 1);
     return nread;
 }
 
 
-
+// This program tries to open a file given as the argument and the read at many offsets for a range 
+// of bytes. This array like reading of file is done using lseek command.
+// It also uses two different ways to call lseek.
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Usage: %s input_file\n", argv[0]);
@@ -81,7 +71,7 @@ int main(int argc, char *argv[]) {
 
     int fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
-        char error_msg[100];
+        char error_msg[MAX_BUFFER_SIZE];
         snprintf(error_msg, sizeof(error_msg), "Error while opening file %s", argv[1]);
         perror(error_msg);
         exit(1);
@@ -94,16 +84,13 @@ int main(int argc, char *argv[]) {
     int total_cycles = 0;
     ssize_t nread = 0;
     off_t offset = 0;
-    const int jump = 5;
+    const int offset_jump = 5;
     do {
         nread = 0;
         int nbytes = 1;
         const int nbytes_jump = 5;
-        int prev_nread = nread;
-        do {
+        while (1) {
             total_cycles += 1;
-            prev_nread = nread;
-            // printf("\n");
             
             char buffer1[MAX_BUFFER_SIZE] = "";
             const ssize_t nread1 = seek_set_opened_file(fd, offset, buffer1, nbytes);
@@ -121,13 +108,18 @@ int main(int argc, char *argv[]) {
             if (strcmp(buffer1, buffer2) != 0) {
                 printf("ERROR: Buffer mismatch\n");
             }
-            
-            // printf("\n");
+            if (nread < nbytes) {
+                // For this offset the buffer was bigger than the amount of data available. It is 
+                // of no use to further increase the buffer size. This time to take a break!
+                break;
+            }
             
             nbytes += nbytes_jump;
-            // printf("  nread = %d, prev_nread = %d\n", nread, prev_nread);
-        } while (nread > prev_nread); // Increase buffer until we don't read more than previous
-        offset += jump;
+        }
+        
+        offset += offset_jump;
+        
+        printf("\n"); // New offset vertical separator in the table
     } while (nread > 0);
 
     printf("\n\nTotal cycles = %d\n\n", total_cycles);
