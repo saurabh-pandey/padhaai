005241a42335fd011d6a2ecf3de620026d50cea7
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-08-28 22:22:05 +0530

    TCPL-CH08: Updated lseek trials test

--- the_c_prog_lang/ch08/tests/test_lseek_trials.py
+++ the_c_prog_lang/ch08/tests/test_lseek_trials.py
@@ -13,9 +13,35 @@ class TestLseekTrials(unittest.TestCase):
         result = subprocess.run([self.exe, "tests/data/text.txt"],
                                 capture_output=True,
                                 text=True)
-        print(result.stdout)
-        # self.assertEqual(result.stdout,
-        #                  "Error while opening tests/data/input1.txt: No such file or directory\n")
+        # print(result.stdout)
+        rows = []
+        for line in result.stdout.splitlines():
+            line.strip()
+            if not line or line.startswith("=") or line.startswith("index"):
+                continue
+            # print(line)
+            parts = line.split()
+            if len(parts) == 4:
+                idx, offset, bytes_, nread = map(int, parts)
+                rows.append((idx, offset, bytes_, nread))
+        # print()
+        # print(rows)
+        here = pathlib.Path(__file__).parent
+        data_file = here / "data" / "expected.txt"
+        file_rows = []
+        with open(data_file) as expected_f:
+            # print(expected_f.readlines())
+            for line in expected_f.readlines():
+                line.strip()
+                if not line or line.startswith("=") or line.startswith("index"):
+                    continue
+                # print(line)
+                parts = line.split()
+                if len(parts) == 4:
+                    idx, offset, bytes_, nread = map(int, parts)
+                    file_rows.append((idx, offset, bytes_, nread))
+        # print(file_rows)
+        self.assertEqual(rows, file_rows)
     
     def test_bad_input(self):
         result = subprocess.run([self.exe, "tests/data/input1.txt"],
