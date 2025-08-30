19368ee493e89dac3c5b2f2ffd241bb5cad3851c
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-08-31 00:19:42 +0530

    TCPL-CH08: Some checks added to ensure all expected arguments are passed

--- the_c_prog_lang/ch08/read_nbytes_offset.c
+++ the_c_prog_lang/ch08/read_nbytes_offset.c
@@ -9,23 +9,25 @@
 // #include <string.h>
 
 
-int read_nbytes_from_pos_seek_set(int fd, off_t pos, size_t nbytes, char *buf) {
-    return 0;
-}
-
-
-int read_nbytes_from_pos_seek_data(int fd, off_t pos, size_t nbytes, char *buf) {
-    return 0;
-}
-
-
 void print_usage(int is_error, const char *argv_0) {
-    fprintf((is_error) ? stderr : stdout, "Usage: %s [-h] [-v] [-f file]\n", argv_0);
-    fprintf((is_error) ? stderr : stdout, "  -h  Help\n");
-    fprintf((is_error) ? stderr : stdout, "  -v  Enable SEEK_DATA version (Default SEEK_SET)\n");
-    fprintf((is_error) ? stderr : stdout, "  -f  File to seek\n");
-    fprintf((is_error) ? stderr : stdout, "\nExample: %s -v -f some_file.txt\n\n", argv_0);
+    fprintf((is_error) ? stderr : stdout,
+            "Usage: %s [-h] [-v] [-f file] [-n nbytes] [-o offset]\n",
+             argv_0);
+    fprintf((is_error) ? stderr : stdout,
+            "  -h  Help\n");
+    fprintf((is_error) ? stderr : stdout,
+            "  -v  Enable SEEK_DATA version (Default SEEK_SET)\n");
+    fprintf((is_error) ? stderr : stdout,
+            "  -f  File to seek\n");
+    fprintf((is_error) ? stderr : stdout,
+            "  -n  Maximum number of bytes to be read. Must be positive number\n");
+    fprintf((is_error) ? stderr : stdout,
+            "  -o  Offset from where to start reading. Must be non-negative\n");
+    fprintf((is_error) ? stderr : stdout,
+            "\nExample: %s -v -f some_file.txt -n 16 -o 10\n\n",
+            argv_0);
 }
+
 
 int string_to_size_t(const char *input, size_t *num) {
     char *endptr = NULL;
@@ -38,6 +40,31 @@ int string_to_size_t(const char *input, size_t *num) {
 
     *num = (size_t)val;
 
+    return 0;
+}
+
+
+int string_to_long(const char *input, off_t *num) {
+    char *endptr = NULL;
+    long long val = strtoll(input, &endptr, 10);
+
+    if (*endptr != '\0') {
+        fprintf(stderr, "Invalid input %s to convert to long\n", input);
+        return -1;
+    }
+
+    *num = (off_t)val;
+
+    return 0;
+}
+
+
+int read_nbytes_from_pos_seek_set(int fd, off_t pos, size_t nbytes, char *buf) {
+    return 0;
+}
+
+
+int read_nbytes_from_pos_seek_data(int fd, off_t pos, size_t nbytes, char *buf) {
     return 0;
 }
 
@@ -73,6 +100,9 @@ int main(int argc, char *argv[]) {
             break;
         case 'o':
             printf("Option o with arg %s\n", optarg);
+            if (string_to_long(optarg, &offset) == -1) {
+                return 1;
+            }
             break;
         default:
             print_usage(1, argv[0]);
@@ -80,13 +110,27 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    printf("argc = %d, optind = %d\n", argc, optind);
+
     if (inp_file == NULL) {
         fprintf(stderr, "\nPlease provide an input file using \"-f some_file\" option\n\n");
         print_usage(1, argv[0]);
         return 1;
     }
 
-    printf("argc = %d, optind = %d\n", argc, optind);
+    if (nbytes == 0) {
+        fprintf(stderr, "\nnbytes has to be a positive number\n\n");
+        print_usage(1, argv[0]);
+        return 1;
+    }
+
+    if (offset >= 0) {
+        fprintf(stderr, "\nOffset must be non-negative\n\n");
+        print_usage(1, argv[0]);
+        return 1;
+    }
+
+    printf("use_seek_data = %d, file = %s, nbytes = %zu, offset = %ld\n", use_seek_data, inp_file, nbytes, offset);
 
     int nread = 0;
     if (use_seek_data) {
