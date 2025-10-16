a8dd53fc9022a03bedccecf7715102cd197a8944
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-10-16 22:29:54 +0530

    TCPL-CH08: Added some ideas on how to get fsize to work

--- the_c_prog_lang/ch08/fsize/fsize.c
+++ the_c_prog_lang/ch08/fsize/fsize.c
@@ -4,4 +4,10 @@
 
 void fsize(char * name) {
     printf("Calling fsize with name = %s\n", name);
+
+    // Check if name points to a file.
+    // If it is a file print the size and return
+    // Else it is a directory.
+    // In the case of dir we list all files inside the dir and run fsize on each entry
+    // Thus fsize calls itself recursively when the name point to a directory
 }
