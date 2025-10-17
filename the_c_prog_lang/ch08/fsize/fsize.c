6f5e1f64ae673f277de325e290f7b27f36be4622
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-10-17 23:50:38 +0530

    TCPL-CH08: Getting stat info about a file or directory

--- the_c_prog_lang/ch08/fsize/fsize.c
+++ the_c_prog_lang/ch08/fsize/fsize.c
@@ -1,8 +1,9 @@
 #include "fsize.h"
 
 #include <stdio.h>
+#include <sys/stat.h>
 
-void fsize(char * name) {
+void fsize(const char * name) {
     printf("Calling fsize with name = %s\n", name);
 
     // Check if name points to a file.
@@ -10,4 +11,19 @@ void fsize(char * name) {
     // Else it is a directory.
     // In the case of dir we list all files inside the dir and run fsize on each entry
     // Thus fsize calls itself recursively when the name point to a directory
+
+    struct stat st;
+    if (stat(name, &st) == -1) {
+        perror("Error in stat");
+        return;
+    }
+    
+    if (S_ISREG(st.st_mode)) {
+        printf("%s is a regular file\n", name);
+        printf("%s size = %ld\n", name, st.st_size);
+    } else if (S_ISDIR(st.st_mode)) {
+        printf("%s is a directory\n", name);
+    } else {
+        printf("%s is something else (socket, symlink, device, etc.).\n", name);
+    }
 }
--- the_c_prog_lang/ch08/fsize/fsize.h
+++ the_c_prog_lang/ch08/fsize/fsize.h
@@ -1,6 +1,6 @@
 #ifndef TCPL_CH08_FSIZE_FSIZE_H
 #define TCPL_CH08_FSIZE_FSIZE_H
 
-void fsize(char * name);
+void fsize(const char * name);
 
 #endif // TCPL_CH08_FSIZE_FSIZE_H
