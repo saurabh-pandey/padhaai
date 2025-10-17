e648550ec1da00119dc9f2b9b365e622696c7c00
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-10-17 23:55:44 +0530

    TCPL-CH08: Removed some comment ideas

--- the_c_prog_lang/ch08/fsize/fsize.c
+++ the_c_prog_lang/ch08/fsize/fsize.c
@@ -6,12 +6,6 @@
 void fsize(const char * name) {
     printf("Calling fsize with name = %s\n", name);
 
-    // Check if name points to a file.
-    // If it is a file print the size and return
-    // Else it is a directory.
-    // In the case of dir we list all files inside the dir and run fsize on each entry
-    // Thus fsize calls itself recursively when the name point to a directory
-
     struct stat st;
     if (stat(name, &st) == -1) {
         perror("Error in stat");
