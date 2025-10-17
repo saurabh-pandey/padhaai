bc25c27e86bd5524a6d3485c5b39b9b91c52d932
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-10-18 00:45:56 +0530

    TCPL-CH08: Reading a directory file for the contents

--- the_c_prog_lang/ch08/fsize/fsize.c
+++ the_c_prog_lang/ch08/fsize/fsize.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
+#include <dirent.h>
+
+
 void fsize(const char * name) {
     printf("Calling fsize with name = %s\n", name);
 
@@ -17,6 +20,19 @@ void fsize(const char * name) {
         printf("%s size = %ld\n", name, st.st_size);
     } else if (S_ISDIR(st.st_mode)) {
         printf("%s is a directory\n", name);
+        DIR *dir = opendir(name);
+        if (!dir) {
+            perror("opendir");
+            return;
+        }
+
+        printf("Listing contents of dir: %s\n", name);
+        struct dirent *entry;
+        while ((entry = readdir(dir)) != NULL) {
+            printf("  %s\n", entry->d_name);
+        }
+
+        closedir(dir);
     } else {
         printf("%s is something else (socket, symlink, device, etc.).\n", name);
     }
