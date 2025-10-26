3e44b9d60c8f87922daa46767e7fcdddc67c3327
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-10-26 21:41:12 +0530

    TCPL-CH08: Done with fsize tests

--- the_c_prog_lang/ch08/fsize/tests/test_fsize.py
+++ the_c_prog_lang/ch08/fsize/tests/test_fsize.py
@@ -2,6 +2,25 @@ import pathlib
 import subprocess
 import unittest
 
+import os
+
+def expected_file_size_map(path):
+    expected_map = {}
+    for root, _, files in os.walk(path):
+        for name in files:
+            filepath = os.path.join(root, name)
+            if os.path.isfile(filepath):  # avoid broken links, etc.
+                sz = os.path.getsize(filepath)
+                expected_map[filepath] = sz
+    return expected_map
+
+def actual_file_size_map(output: str):
+    actual_map = {}
+    lines = output.splitlines()
+    for line in lines[1:]:
+        split_lines = line.split()
+        actual_map[split_lines[0]] = int(split_lines[-1])
+    return actual_map
 
 class TestFsize(unittest.TestCase):
     @classmethod
@@ -13,19 +32,17 @@ class TestFsize(unittest.TestCase):
         output = subprocess.run([self.exe],
                                 capture_output=True,
                                 text=True).stdout
-        # print(output.splitlines())
-        for line in output.splitlines():
-            print(line)
-        # self.assertEqual(output.splitlines(), ['Hello world', 'Now this is a newline'])
+        expected = expected_file_size_map(".")
+        actual = actual_file_size_map(output)
+        self.assertDictEqual(expected, actual, "File size don't match for the no args test")
     
     def test_wi_arg(self):
         output = subprocess.run([self.exe, "dir_test"],
                                 capture_output=True,
                                 text=True).stdout
-        # print(output.splitlines())
-        for line in output.splitlines():
-            print(line)
-        # self.assertEqual(output.splitlines(), ['Hello world', 'Now this is a newline'])
+        expected = expected_file_size_map("dir_test")
+        actual = actual_file_size_map(output)
+        self.assertDictEqual(expected, actual, "File size don't match for the args test")
 
 
 if __name__ == '__main__':
