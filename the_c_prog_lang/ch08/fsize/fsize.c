fa7c682c4f139777f56bc88adbec79c38fd2f80d
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-10-26 14:28:55 +0530

    TCPL-CH08: Cleanup of fsize

--- the_c_prog_lang/ch08/fsize/fsize.c
+++ the_c_prog_lang/ch08/fsize/fsize.c
@@ -6,12 +6,8 @@
 
 #include <dirent.h>
 
-// #define MAX_PATH 100
-
 
 void fsize(const char * name, int depth) {
-    // printf("Calling fsize with name = %s\n", name);
-
     struct stat st;
     if (stat(name, &st) == -1) {
         perror("Error in stat");
@@ -19,28 +15,23 @@ void fsize(const char * name, int depth) {
     }
     
     if (S_ISREG(st.st_mode)) {
-        // printf("%s is a regular file\n", name);
         for (int i = 0; i < depth; ++i) {
             printf(" ");
         }
         printf("%s size = %ld\n", name, st.st_size);
     } else if (S_ISDIR(st.st_mode)) {
-        // printf("%s is a directory\n", name);
         DIR *dir = opendir(name);
         if (!dir) {
             perror("opendir");
             return;
         }
 
-        // printf("Listing contents of dir: %s\n", name);
         struct dirent *entry;
         while ((entry = readdir(dir)) != NULL) {
-            // printf("  %s\n", entry->d_name);
             char dir_path[PATH_MAX];
 
             sprintf(dir_path, "%s/%s", name, entry->d_name);
 
-            // printf("  Path: %s\n", dir_path);
             if ((strcmp(entry->d_name, ".") != 0) && (strcmp(entry->d_name, "..") != 0))
             {
                 fsize(dir_path, depth + 2);
