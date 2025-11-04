4a5573ce9d3061767347155671774a9b29ad19ae
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-11-04 22:38:13 +0530

    TCPL-CH08: Trying to understand sbrk sys call

--- the_c_prog_lang/ch08/storage_allocator/main.c
+++ the_c_prog_lang/ch08/storage_allocator/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 
 int main(int argc, char *argv[]) {
     printf("Running storage allocator\n");
+
+    void *brk = sbrk(0);
+    printf("Before malloc Program break: %p\n", brk);
+
+    int * data = (int *)malloc(1000000 * sizeof(int));
+
+    brk = sbrk(0);
+    printf("After malloc Program break : %p\n", brk);
+
+    free(data);
+
+    brk = sbrk(0);
+    printf("After free Program break   : %p\n", brk);
+
+    brk = sbrk(1024*100);
+    printf("After calling sbrk(64)     : %p\n", brk);
     
     return 0;
 }
