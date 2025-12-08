83b1aeb0abb29e2f2faf2e6992f0fbe6630b1cc5
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-12-08 21:58:38 +0530

    TCPL-CH08: Adding a simple object pool

--- the_c_prog_lang/ch08/storage_allocator/vec_object_pool.c
+++ the_c_prog_lang/ch08/storage_allocator/vec_object_pool.c
@@ -11,23 +11,56 @@ Link to the video is: https://www.youtube.com/watch?v=CpgsQLSc7KY
 
 typedef struct
 {
-    int X;
+    int x;
     int y;
     int z;
 } Vector;
 
 #define MAX_POOL_SIZE 10
 
-Vector pool[MAX_POOL_SIZE];
+typedef struct {
+    int is_free;
+    Vector v;
+} PoolItem;
+
+PoolItem pool[MAX_POOL_SIZE] = {{1, {-1, -1, -1}}};
 
 Vector * borrow(void) {
+    for (int i = 0; i < sizeof(pool)/sizeof(PoolItem); ++i) {
+        if (pool[i].is_free == 1) {
+            pool[i].is_free = 0;
+            return &(pool[i].v);
+        }
+    }
     return NULL;
 }
 
-void yield(Vector * v) {
+int yield(Vector * v) {
+    if (v == NULL) {
+        return -1;
+    }
+    
+    for (int i = 0; i < sizeof(pool)/sizeof(PoolItem); ++i) {
+        if (v == &(pool[i].v)) {
+            if (pool[i].is_free != 0) {
+                printf("ERROR: Returned object is already marked free\n");
+                return -1;
+            }
+            pool[i].is_free = 1;
+            return i;
+        }
+    }
+    
+    return -1;
 }
 
-
+void print_vec(const Vector * v) {
+    if (v == NULL) {
+        printf("{NULL}\n");
+    } else {
+        printf("{%d, %d, %d}\n", v->x, v->y, v->z);
+    }
+}
 
 int main(int argc, char * argv[]) {
     printf("Testing Vector memory pool\n");
@@ -36,7 +69,14 @@ int main(int argc, char * argv[]) {
 
     for (int i = 0; i < max_trials; ++i) {
         Vector * v = borrow();
-        // Do something with v
+        if (v != NULL) {
+            v->x = i;
+            v->y = i;
+            v->z = i;
+        }
+
+        print_vec(v);
+        
         yield(v);
     }
 
