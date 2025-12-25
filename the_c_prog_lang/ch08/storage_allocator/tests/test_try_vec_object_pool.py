9e6e7f4eed5e7e379e77f4ceafb47eeba23963a2
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-12-25 23:57:55 +0530

    TCPL-CH08: Included freelist version test

--- the_c_prog_lang/ch08/storage_allocator/tests/test_try_vec_object_pool.py
+++ the_c_prog_lang/ch08/storage_allocator/tests/test_try_vec_object_pool.py
@@ -7,19 +7,28 @@ class TestTryVecObjectPool(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         p = pathlib.Path(__file__)
-        cls.const_exe = pathlib.Path(p.parents[1] / 'bin' / (p.stem[len("test_"):] + "_constant"))
-        cls.linear_exe = pathlib.Path(p.parents[1] / 'bin' / (p.stem[len("test_"):] + "_linear"))
+        cls.const_exe = pathlib.Path(p.parents[1] / 'bin' / "try_vec_linear_pool_constant")
+        cls.linear_exe = pathlib.Path(p.parents[1] / 'bin' / "try_vec_linear_pool_linear")
+        cls.freelist_exe = pathlib.Path(p.parents[1] / 'bin' / "try_vec_freelist_pool")
 
     def test_run(self):
         const_output = subprocess.run([self.const_exe], capture_output=True, text=True).stdout
         linear_output = subprocess.run([self.linear_exe], capture_output=True, text=True).stdout
+        freelist_output = subprocess.run([self.freelist_exe], capture_output=True, text=True).stdout
         const_result = self._parse_output(const_output)
         linear_result = self._parse_output(linear_output)
+        freelist_result = self._parse_output(freelist_output)
+        self.assertEqual(len(const_result), len(linear_result))
+        self.assertEqual(len(const_result), len(freelist_result))
         for key in const_result:
             self.assertTrue(key in linear_result, f"{key} not found in linear output result")
+            self.assertTrue(key in freelist_result, f"{key} not found in freelist output result")
             self.assertGreaterEqual(linear_result[key],
                                     const_result[key],
-                                    f"{key} result is slower")
+                                    f"Constant {key} result is slower")
+            self.assertGreaterEqual(const_result[key],
+                                    freelist_result[key],
+                                    f"Freelist {key} result is slower")
     
     def _parse_output(self, output):
         result = {}
