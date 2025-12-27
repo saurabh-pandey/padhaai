2c8e2895e39f1b6eb383fca9bd8b6ced0703a693
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-12-27 12:57:30 +0530

    TCPL-CH08: Preparing for video version of freelist

---
+++ the_c_prog_lang/ch08/storage_allocator/freelist_vec_pool_v1.c
@@ -0,0 +1,77 @@
+/* This file contains a fixed size object pool. It serves only a Vector object and can accept it 
+back into the pool.
+This simple fixed size vec object pool serves as a simpler example towards understand a generic
+storage allocator.
+It is based on the following youtube video by Jacob Sorber titled "Making allocators and object pools faster using a free list".
+Link to the video is: https://www.youtube.com/watch?v=MxgnS9Lwv0k
+*/
+
+#include "freelist_vec_pool.h"
+
+#include <stdio.h>
+
+
+#define MAX_POOL_SIZE 10000
+#define MAX_BORROWS MAX_POOL_SIZE + 100
+
+
+Vector vector_pool[MAX_POOL_SIZE];
+const Vector init = {-1, -1, -1};
+
+typedef struct FreeList {
+    size_t vec_id;
+    struct FreeList * next;
+} FreeList;
+
+FreeList pool[MAX_POOL_SIZE];
+
+FreeList * head = NULL;
+
+
+void initialize_pool(void) {
+    for (size_t i = 0; i < MAX_POOL_SIZE - 1; ++i) {
+        vector_pool[i] = init;
+        pool[i].vec_id = i;
+        pool[i].next = &(pool[i]);
+    }
+    pool[MAX_POOL_SIZE - 1].vec_id = MAX_POOL_SIZE - 1;
+    pool[MAX_POOL_SIZE - 1].next = NULL;
+    
+    head = pool;
+}
+
+
+Vector * borrow(void) {
+    if (head == NULL) {
+        return NULL;
+    }
+    Vector * v = &(vector_pool[head->vec_id]);
+
+    FreeList * old_head = head;
+
+    head = head->next;
+    old_head->next = NULL;
+    return v;
+}
+
+
+int yield(Vector * v) {
+    if (v == NULL) {
+        return -1;
+    }
+    
+    size_t id = (v - vector_pool) / sizeof(vector_pool[0]);
+
+    if (id > (MAX_POOL_SIZE - 1)) {
+        printf("ERROR: Vector to be yielded is out-of-bounds\n");
+        return -1;
+    }
+    
+    FreeList * fl = &(pool[id]);
+    // printf("New free list next = %p\n", fl->next);
+    fl->next = head;
+    head = fl;
+
+    return id;
+}
+
--- the_c_prog_lang/ch08/storage_allocator/tests/test_try_vec_object_pool.py
+++ the_c_prog_lang/ch08/storage_allocator/tests/test_try_vec_object_pool.py
@@ -9,30 +9,49 @@ class TestTryVecObjectPool(unittest.TestCase):
         p = pathlib.Path(__file__)
         cls.const_exe = pathlib.Path(p.parents[1] / 'bin' / "try_vec_linear_pool_constant")
         cls.linear_exe = pathlib.Path(p.parents[1] / 'bin' / "try_vec_linear_pool_linear")
-        cls.freelist_exe = pathlib.Path(p.parents[1] / 'bin' / "try_vec_freelist_pool")
+        cls.freelist_exe_v1 = pathlib.Path(p.parents[1] / 'bin' / "try_vec_freelist_pool_v1")
+        cls.freelist_exe_v2 = pathlib.Path(p.parents[1] / 'bin' / "try_vec_freelist_pool_v2")
 
     def test_run(self):
         const_output = subprocess.run([self.const_exe], capture_output=True, text=True).stdout
         linear_output = subprocess.run([self.linear_exe], capture_output=True, text=True).stdout
-        freelist_output = subprocess.run([self.freelist_exe], capture_output=True, text=True).stdout
+        freelist_output_v1 = subprocess.run([self.freelist_exe_v1],
+                                            capture_output=True,
+                                            text=True).stdout
+        freelist_output_v2 = subprocess.run([self.freelist_exe_v2],
+                                            capture_output=True,
+                                            text=True).stdout
         const_result = self._parse_output(const_output)
         linear_result = self._parse_output(linear_output)
-        freelist_result = self._parse_output(freelist_output)
+        freelist_result_v1 = self._parse_output(freelist_output_v1)
+        freelist_result_v2 = self._parse_output(freelist_output_v2)
+        print()
+        print("FL V1", freelist_result_v1)
+        print("FL V2", freelist_result_v2)
         self.assertEqual(len(const_result),
                          len(linear_result),
                          "Constant and linear results size mismatch")
         self.assertEqual(len(const_result),
-                         len(freelist_result),
-                         "Constant and Freelist results size mismatch")
+                         len(freelist_result_v1),
+                         "Constant and Freelist V1 results size mismatch")
+        self.assertEqual(len(const_result),
+                         len(freelist_result_v2),
+                         "Constant and Freelist V2 results size mismatch")
         for key in const_result:
             self.assertTrue(key in linear_result, f"{key} not found in linear output result")
-            self.assertTrue(key in freelist_result, f"{key} not found in freelist output result")
+            self.assertTrue(key in freelist_result_v1,
+                            f"{key} not found in freelist v1 output result")
+            self.assertTrue(key in freelist_result_v2,
+                            f"{key} not found in freelist v2 output result")
             self.assertGreaterEqual(linear_result[key],
                                     const_result[key],
                                     f"Constant {key} result is slower")
             self.assertGreaterEqual(const_result[key],
-                                    freelist_result[key],
-                                    f"Freelist {key} result is slower")
+                                    freelist_result_v1[key],
+                                    f"Freelist V1 {key} result is slower")
+            self.assertGreaterEqual(const_result[key],
+                                    freelist_result_v2[key],
+                                    f"Freelist V2 {key} result is slower")
     
     def _parse_output(self, output):
         result = {}
