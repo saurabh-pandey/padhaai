cd0d7d8e568538eb6ce08e6856b46ba78f1be8c2
Saurabh Pandey <saurabhpandey85@gmail.com>
2025-12-30 23:41:41 +0530

    TCPL-CH08: Starting with the implementation of simple allocator

--- the_c_prog_lang/ch08/storage_allocator/simple_allocator.c
+++ the_c_prog_lang/ch08/storage_allocator/simple_allocator.c
@@ -1,11 +1,87 @@
 #include "simple_allocator.h"
 
-#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
 
-void * my_malloc(unsigned bytes) {
-    return NULL;
-}
+#define NALLOC 10
+
+typedef union Header {
+    struct {
+        union Header * next;
+        size_t sz;
+    } s;
+    max_align_t block;
+} Header;
+
+Header * freep = NULL;
+
 
 void my_free(void * p) {
+    // Find the block this memory belongs to and insert
+}
 
+void * morecore(size_t nunits) {
+    void * new_mem = sbrk(nunits * sizeof(Header));
+    if (new_mem == (void *)-1) {
+        perror("sbrk failed");
+        return NULL;
+    }
+
+    Header * new_block = (Header *)new_mem;
+    new_block->s.sz = nunits;
+    
+    // Assign this to the list ordered by memory
+    my_free(new_mem);
+    
+    return new_mem;
+}
+
+void * my_malloc(size_t nbytes) {
+    const size_t nunits = (nbytes / sizeof(Header)) + 2;
+    
+    if (freep == NULL) {
+        // This is the first call or everything was allocated
+        freep = morecore(nunits);
+    }
+    
+    // Need only 2 steps.
+    // Step 1 if the first fit block is found in the existing list
+    // Step 2 if nothing was found create a new one and return the block
+    for (int i = 0; i < 2; ++i) {
+        Header * curr = freep;
+        Header * prev = NULL;
+        while(curr != NULL) {
+            if (curr->s.sz >= nunits) {
+                // Found the first fit block
+                if (curr->s.sz == nunits) {
+                    // Size matches perfectly just plug out the block and return
+                    if (prev != NULL) {
+                        // Remove the block from the list
+                        prev->s.next = curr->s.next;
+                    } else {
+                        // This was the first block so freep is pointing to the next one 
+                        freep = curr->s.next;
+                    }
+                    return (void *)(curr + 1);
+                } else {
+                    // This block is bigger so resize and return the tail end
+                    curr->s.sz -= nunits;
+                    // TODO: Resize here
+                }
+            }
+            curr = curr->s.next;
+            prev = curr;
+        }
+
+        // If here then nothing of the right size was found
+        // Add a block to the list
+        morecore(nunits);
+        // And search again
+        // TODO: This is an overkill. I know this is the block to be returned so why search?
+        // If we know curr and prev ptr then above loop can be used directly. Just reset curr and
+        // prev to this new block and continue the loop
+    }
+
+    // Not enough memory so error case
+    return NULL;
 }
--- the_c_prog_lang/ch08/storage_allocator/simple_allocator.h
+++ the_c_prog_lang/ch08/storage_allocator/simple_allocator.h
@@ -1,7 +1,9 @@
 #ifndef TCPL_CH08_STORAGE_ALLOCATOR_SIMPLE_ALLOCATOR_H
 #define TCPL_CH08_STORAGE_ALLOCATOR_SIMPLE_ALLOCATOR_H
 
-void * my_malloc(unsigned bytes);
+#include <stddef.h>
+
+void * my_malloc(size_t nbytes);
 
 void my_free(void * p);
 
