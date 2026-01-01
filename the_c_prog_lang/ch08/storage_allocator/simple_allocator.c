7f34380a6d5628c9cbaa1acc40c94f4cab6e08e8
Saurabh Pandey <saurabhpandey85@gmail.com>
2026-01-01 14:38:01 +0530

    TCPL-CH08: Single malloc and free works

--- the_c_prog_lang/ch08/storage_allocator/simple_allocator.c
+++ the_c_prog_lang/ch08/storage_allocator/simple_allocator.c
@@ -27,11 +27,20 @@ void my_free(void * p) {
     // 3. Or is merged with next
     // 4. Or search in the next pair
     // 5. If the new block address is less than curr that means it is the new head node
-    Header * new_block = (Header *)p;
+    printf("\nmy_free\n");
+
+    // TODO: I think here I should cast the header 1 before the pointer as that is where the actual
+    // header is present
+    printf("Free pointer  = %p\n", p);
+    Header * new_block = (Header *)p - 1;
+    printf("New block ptr = %p\n", new_block);
+    printf("Free block sz = %zu\n", new_block->s.sz);
+
     Header * curr = freep;
     while (curr != NULL) {
         if (new_block < curr) {
             // This has to be new head node
+            printf("New head added\n");
             new_block->s.next = freep;
             freep = new_block;
             return;
@@ -67,18 +76,24 @@ void my_free(void * p) {
     // If here then must likely head is NULL
     if (freep != NULL) {
         printf("Head was supposed to be NULL :(\n");
+    } else {
+        printf("Head is NULL as expected so adding the freed head block\n");
     }
     // In this case this is the new head
     freep = new_block;
 }
 
 
-void * morecore(size_t nunits) {
+void * my_morecore(size_t nunits) {
+    printf("\nmy_morecore\n");
     void * new_mem = sbrk(nunits * sizeof(Header));
     if (new_mem == (void *)-1) {
         perror("sbrk failed");
         return NULL;
     }
+
+    printf("Original program break = %p\n", new_mem);
+    printf("New program break      = %p\n", sbrk(0));
 
     Header * new_block = (Header *)new_mem;
     new_block->s.sz = nunits;
@@ -91,11 +106,17 @@ void * morecore(size_t nunits) {
 
 
 void * my_malloc(size_t nbytes) {
+    printf("\nmy_malloc\n");
+    printf("Size of Header = %zu\n", sizeof(Header));
+    printf("Bytes requested = %zu\n", nbytes);
+    
     const size_t nunits = (nbytes / sizeof(Header)) + 2;
+    printf("nunits = %zu\n", nunits);
     
     if (freep == NULL) {
         // This is the first call or everything was allocated
-        freep = morecore(nunits);
+        printf("First call or no memory left\n");
+        freep = my_morecore(nunits);
     }
     
     // Need only 2 steps.
@@ -105,10 +126,12 @@ void * my_malloc(size_t nbytes) {
         Header * curr = freep;
         Header * prev = NULL;
         while(curr != NULL) {
+            printf("  Curr size = %zu\n", curr->s.sz);
             if (curr->s.sz >= nunits) {
                 // Found the first fit block
                 if (curr->s.sz == nunits) {
                     // Size matches perfectly just plug out the block and return
+                    printf("    Found exact matching block\n");
                     if (prev != NULL) {
                         // Remove the block from the list
                         prev->s.next = curr->s.next;
@@ -116,20 +139,26 @@ void * my_malloc(size_t nbytes) {
                         // This was the first block so freep is pointing to the next one 
                         freep = curr->s.next;
                     }
-                    return (void *)(curr + 1);
+                    return (curr + 1);
                 } else {
                     // This block is bigger so resize and return the tail end
+                    printf("  Found bigger block\n");
                     curr->s.sz -= nunits;
                     // TODO: Resize here
+                    Header * tail_block = (Header *)curr + curr->s.sz;
+                    tail_block->s.sz = nunits;
+                    return (tail_block + 1);
                 }
             }
+            printf("  Searching in next\n");
             curr = curr->s.next;
             prev = curr;
         }
 
         // If here then nothing of the right size was found
         // Add a block to the list
-        morecore(nunits);
+        printf("No block fit for this request found so adding more memory\n");
+        my_morecore(nunits);
         // And search again
         // TODO: This is an overkill. I know this is the block to be returned so why search?
         // If we know curr and prev ptr then above loop can be used directly. Just reset curr and
@@ -137,5 +166,6 @@ void * my_malloc(size_t nbytes) {
     }
 
     // Not enough memory so error case
+    printf("ERROR: Seems like we are out of memory\n");
     return NULL;
 }
--- the_c_prog_lang/ch08/storage_allocator/try_storage_allocator.c
+++ the_c_prog_lang/ch08/storage_allocator/try_storage_allocator.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 // Comment to try the new allocator
-#define DEBUG
+// #define DEBUG
 
 #ifdef DEBUG
 // For testing only
@@ -18,9 +18,33 @@
 #endif
 
 
-int main() {
-    printf("Running storage allocator\n");
+void test_multiple_malloc_free() {
+    const char * orig_hello_world = "Hello World";
+    char *my_hello_world = (char *)MALLOC(12);
 
+    for (int i = 0; i < 12; ++i) {
+        my_hello_world[i] = orig_hello_world[i];
+    }
+
+    printf("Original str = %s\n", orig_hello_world);
+    printf("New str      = %s\n", my_hello_world);
+
+    const char * orig_welcome_malloc = "Welcome My Malloc";
+    char *my_welcome_malloc = (char *)MALLOC(18);
+
+    for (int i = 0; i < 18; ++i) {
+        my_welcome_malloc[i] = orig_welcome_malloc[i];
+    }
+
+    printf("Original str = %s\n", orig_welcome_malloc);
+    printf("New str      = %s\n", my_welcome_malloc);
+
+    FREE(my_hello_world);
+    FREE(my_welcome_malloc);
+}
+
+
+void test_single_malloc_free() {
     const char * hello_world = "Hello World";
     char *str = (char *)MALLOC(12);
 
@@ -32,6 +56,17 @@ int main() {
     printf("New str      = %s\n", str);
 
     FREE(str);
+}
+
+
+int main() {
+    printf("Running storage allocator\n");
+
+    test_single_malloc_free();
+
+    test_single_malloc_free();
+
+    // test_multiple_malloc_free();
 
     return 0;
 }
